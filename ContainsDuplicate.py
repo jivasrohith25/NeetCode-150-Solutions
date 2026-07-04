7490209a0d2207b9cdd2f2ef3377908fd172e1cf
rohith <rohith@example.com>
2026-07-04 15:29:05 +0530

    NeetCode 1

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -1,6 +1,7 @@
-from typing import List
-
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    return True
+        return False
